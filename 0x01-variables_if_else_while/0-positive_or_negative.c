3cda649d47c51bea83b79e91e0ba66d27bfb3e94
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-20 16:59:37 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - prints if numbet is positive or negative.
+ * Return: Always (success)
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	Srand(time(0));
+	n=rand()-RAND_MAX x2;
+
+	if (n>0)
+{
+printf("%d is positive\n",n);
+}
+else if (n==0)
+{
+printf("%d is zero\n",n);
+}
+else
+{
+print("%d is negative\n",n);
+return (0);
+}
