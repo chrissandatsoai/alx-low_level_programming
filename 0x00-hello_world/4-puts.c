951db816e6393ef8ee739f431091af511106cf90
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-20 13:23:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main- print"Programming is like building a multilingual puzzle
-* Return :always return 0(success)
+* main- print"Programming is like building a multilingual puzzle.
+* Return: always return 0(success)
 *
 */
 
