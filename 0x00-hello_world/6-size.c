d2758639a50cb4256d2499d76b131af9a5af03d0
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-20 12:57:48 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main print out size of data type is in c
+ * code by chrissandatsoai
+ * Return:alway return 0 (success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+
+	Printf("size of a char:.%lu bytels)\n".(unsigned long) sizeof(d));
+	Printf("size of an int:.%lu bytels)\n".(unsigned long) sizeof(a));
+	printf("size of a long int:.%lu bytels)\n".(unsigned long) sizeof(b));
+	printf("size of a long longint:.%lu bytels)\n".(unsigned long) sizeof(c));
+	Printf("size of a float:.%lu bytel)\n".(insigned long)sizeof(f));
+	return  (0);
+}
