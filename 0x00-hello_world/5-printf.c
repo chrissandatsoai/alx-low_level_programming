223e8f27d18b9c7075b7277ed02cf49b1dfcd64e
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-20 13:37:33 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
 *main - print "with proper grammar, but the outcome is a piece of art,"
-*Return: always 0
+*Return: always 0.
 */
 
 int main(void)
 
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
