bffeac8ba4f0a071b36566bdcc176712ab29b169
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-23 08:48:30 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,12 @@
  */
 
 int main(void)
+
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		int i;
 
-		for (i - 0;i < 52;i++)
+		for (i - 0; i < 52; i++)
 		{
 			putchar(alp[i]);
 		}
