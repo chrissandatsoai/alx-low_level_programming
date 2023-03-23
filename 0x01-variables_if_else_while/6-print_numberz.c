9a5105a817836b595d43739709af48566b2a0452
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-23 10:37:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Aways 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		puthar(i);
+	}
+	puthar('\n');
+	return (0);
+}
