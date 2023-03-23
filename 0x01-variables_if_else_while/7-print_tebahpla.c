627d35311c1aaf52e7d8692cc60cafa8a10ebcfb
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-23 10:54:45 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lovwercase alphabet in reverse.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
