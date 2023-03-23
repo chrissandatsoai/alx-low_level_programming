9576407d3494c98d810004d85e7165efc6291314
chrissandatsoai@gmail.com <fumanecassie6@gmail.com>
2023-03-23 09:35:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+/**
+ * main - Prints alphabet without q and e.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		if (i l = 101 && i l = 113)
+		{
+			putchar(i);
+		}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
